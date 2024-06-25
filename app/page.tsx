80b7a874cccc622914808876db2490c3dd6a0c91
Isaac Sutton-Wild <isaacsw999@gmail.com>
2024-06-25 14:26:17 +0100

    style: Refactor form layout and styling for better alignment and readability.

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,35 @@ export default function Home() {
             alt="example of a brat cover"
           ></Image>
           <button>Upload Image</button>
-          <form action='' method="get" className={styles.form}>
-            <label>Tag Text</label>
-            <input type="text" id="tag-text"></input>
-          </form>
+          <div className={styles.center}>
+            <form>
+              <div className={styles.form}>
+                <label>Tag Text</label>
+                <input type="text" id="tag-text"></input>
+              </div>
+              <h4>Tag Position</h4>
+              <div className={styles.form}>
+                <label htmlFor="tag-right">Right</label>
+                <input
+                  type="radio"
+                  name="position"
+                  id="tag-right"
+                  value="right"
+                ></input>
+
+                <label htmlFor="tag-left">Left</label>
+                <input
+                  type="radio"
+                  name="position"
+                  id="tag-left"
+                  value="left"
+                ></input>
+              </div>
+              <div className={styles.formSub}>
+                <input type="submit" value="Submit"></input>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </main>
